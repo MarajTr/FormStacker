cc8a5cd242a41a24ba39e532c08b28d0d55259fc
Maraj Talukder <marajtalukder2000@gmail.com>
2025-04-24 01:45:51 +0600

    cors array

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,26 @@ const  supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
 );
+
+const allowedOrigins = [
+  "https://form-stacker.vercel.app",
+  "https://form-stacker-3jtw9ntk5-marajtrs-projects.vercel.app",
+  "https://form-stacker-256fkxae8-marajtrs-projects.vercel.app",
+  "http://localhost:5000"
+];
+
 app.use(cors({
-  origin: "*",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed for this origin"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 app.use(express.json());
 app.use('/auth', authRouter)
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
